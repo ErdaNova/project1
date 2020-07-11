e233eeacb6fd5a2eb4eef9e93450953a928f4c97
revus01 <qoalsgks9@kaist.ac.kr>
2020-07-11 18:23:00 +0900

    +permission
    +permission

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.myapplication.ui;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
 import com.google.android.material.tabs.TabLayout;
 
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+
 
 import android.content.Intent;
 import android.database.Cursor;
@@ -15,10 +26,19 @@ import android.net.Uri;
 import android.provider.MediaStore;
 
 import androidx.recyclerview.widget.RecyclerView;
-import android.util.Log;
+
 import android.widget.TextView;
 
+
+
 import java.util.ArrayList;
+import androidx.core.content.ContextCompat;
+
+
+
+import android.Manifest;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity implements itemClickListener{
 
@@ -27,21 +47,70 @@ public class MainActivity extends AppCompatActivity implements itemClickListener
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
     private FragmentPagerAdapter fragmentPagerAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_tab);
 
-        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
-        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        String temp = "";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_CONTACTS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " "; }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        viewPager.setAdapter(fragmentPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+        if (TextUtils.isEmpty(temp) == false) {
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "),1);
+
+        }else {
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+
+
+
+
 
 
 
     }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "권한 허용 : " + permissions[i]);
+                }
+            }
+            ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+            fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+            TabLayout tabLayout = findViewById(R.id.tab_layout);
+            viewPager.setAdapter(fragmentPagerAdapter);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     /**
      * @return
